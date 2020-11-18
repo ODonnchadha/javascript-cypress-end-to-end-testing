bfaa2e5c895ef20c99d1e49d83debc8d6c36e876
ddonahue <ddonahue@questarai.com>
2020-11-18 12:11:33 -0600

    INIT

--- cypress/integration/max_char_input.spec.js
+++ cypress/integration/max_char_input.spec.js
@@ -17,4 +17,16 @@ describe('Textbox with max characters', () => {
         cy.get('input').type('abcdefghijklmnopqrstuvwxyz');
         cy.get('input').should('have.attr', 'value', 'abcdefghijklmno');
     });
+
+    it('Should display the appropriate remaining character count using special Cypress.io attributes', () => {
+        cy.visit('http://localhost:3000/example-3');
+
+        cy.get('[data-cy="last-name-chars-left-count"]').invoke('text').should('equal', '15');
+        cy.get('[data-cy="input-last-name"]').type('abcde');
+
+        cy.get('[data-cy="last-name-chars-left-count"]').invoke('text').should('equal', '10');
+        cy.get('[data-cy="input-last-name"]').type('fghijklmno');
+
+        cy.get('[data-cy="last-name-chars-left-count"]').invoke('text').should('equal', '0');
+    });
 });
